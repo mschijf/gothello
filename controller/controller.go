3e9f0cdf4ae3cbad16bdcbe5e40e0a41969b240f
mschijf <mschijf@gmail.com>
2022-05-01 11:22:03 +0200

    adding gin and swagger functionality

---
+++ controller/controller.go
@@ -0,0 +1,68 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "gothello/docs"
+	"net/http"
+)
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+
+// @Summary      GET ALBUMS
+// @Router /albums [get]
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// @Summary      POST ALBUMS
+// @Router /albums [post]
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// @Summary      GET ER EENTJE
+// @Param        id   path      int  true  "Account ID"
+// @Router /albums/{id} [get]
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+func TestController() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	router.Run("localhost:8080")
+}
--- main.go
+++ main.go
@@ -1,24 +1,23 @@
+// @title Gin Swagger Example API
+// @version 1.0
+// @description This is a sample server server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
+// @schemes http
+
 package main
 
-import (
-	"fmt"
-	"gothello/board"
-	"time"
-)
+import "gothello/controller"
 
 func main() {
-	//var bbWhite uint64
-	//
-	//bbWhite = 7
-	//x := bits.OnesCount64(bbWhite)
-	//fmt.Printf("%064b heeft lengte %d\n", bbWhite, x)
-
-	var bb = board.InitStartBoard()
-
-	for i := 0; i < 14; i++ {
-		currentTime := time.Now()
-		var result = bb.Perft(i)
-		diff := time.Now().Sub(currentTime)
-		fmt.Printf("depth %3d  : %12.6f ms --> %14d\n", i, diff.Seconds(), result)
-	}
+	controller.TestController()
 }
