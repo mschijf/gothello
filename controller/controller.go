23e7ec0a29a1800cdc81e93164db38f13993d7c2
mschijf <mschijf@gmail.com>
2022-05-01 21:35:32 +0200

    - finally, running an html file

--- controller/controller.go
+++ controller/controller.go
@@ -6,69 +6,46 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 	_ "gothello/docs"
 	"net/http"
+	"os"
 )
 
-// albums slice to seed record album data.
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
+const ADDRESS = "localhost"
+const PORT = "8080"
 
-// @Summary      Get all albums
-// @Tags         albums
-// @Router /albums [get]
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
+var router *gin.Engine
 
-// @Summary      Post one album
-// @Description  add by json account
-// @Tags         albums
-// @Accept       json
-// @Produce      json
-// @Param        album  body      album  true  "Add Album"
-// @Success      200      {object}  album
-// @Router /albums [post]
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
+func init() {
+	router = gin.Default()
+	err := router.SetTrustedProxies(nil)
+	if err != nil {
+		println("ERROR: setting trusted proxies not succeeding" + err.Error())
 		return
 	}
-
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
 
-// @Summary      GET ER EENTJE
-// @Tags         albums
-// @Param        id   path      int  true  "Account ID"
-// @Router /albums/{id} [get]
-func getAlbumByID(c *gin.Context) {
-	id := c.Param("id")
+// @Summary      Get Sizes of the Board
+// @Tags         size
+// @Router       /sizes [get]
+func getSizes(c *gin.Context) {
+	sizeRecord := size{MaxRow: 8, MaxCol: 8}
+	c.IndentedJSON(http.StatusOK, sizeRecord)
+}
 
-	// Loop over the list of albums, looking for
-	// an album whose ID value matches the parameter.
-	for _, a := range albums {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+func getHtml(c *gin.Context) {
+	html, err := os.ReadFile("./view/othello.html")
+	if err != nil {
+		panic(err)
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(string(html)))
 }
 
 func TestController() {
-	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.POST("/albums", postAlbums)
-	router.GET("/albums/:id", getAlbumByID)
-
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
-	router.Run("localhost:8080")
+	router.GET("/api/v1/sizes", getSizes)
+	router.GET("/", getHtml)
+	err := router.Run(ADDRESS + ":" + PORT)
+	if err != nil {
+		println("ERROR: running the router/handler not working")
+		return
+	}
 }
---
+++ controller/sizemodel.go
@@ -0,0 +1,6 @@
+package controller
+
+type size struct {
+	MaxRow int `json:"maxRow"`
+	MaxCol int `json:"maxCol"`
+}
