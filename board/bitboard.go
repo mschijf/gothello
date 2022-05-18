f4d2747c492e8aea3896900386cae2170c4d7a60
mschijf <mschijf@gmail.com>
2022-05-18 07:17:46 +0200

    replacing all of perft to unit test re-adding passmove in generateMoves() , but not in generatePositions()

--- board/bitboard.go
+++ board/bitboard.go
@@ -177,6 +177,9 @@ func (bitBoard *BitBoard) GenerateMoves(colorToMove int) []Move {
 		resultList = append(resultList, Move{allCaptures, bbMove})
 		candAll ^= bbMove
 	}
+	if len(resultList) == 0 {
+		resultList = append(resultList, Move{0, 0})
+	}
 	return resultList
 }
 
@@ -213,50 +216,4 @@ func (bitBoard *BitBoard) AllFieldsPlayed() bool {
 
 func (bitBoard *BitBoard) ColorHasWon(color int) bool {
 	return bit64math.BitCount(bitBoard[color]) > bit64math.BitCount(bitBoard[1-color])
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//    depth   0  :     0.000000 ms -->              1
-//    depth   1  :     0.000001 ms -->              4
-//    depth   2  :     0.000001 ms -->             12
-//    depth   3  :     0.000005 ms -->             56
-//    depth   4  :     0.000017 ms -->            244
-//    depth   5  :     0.000100 ms -->           1396
-//    depth   6  :     0.000515 ms -->           8200
-//    depth   7  :     0.003094 ms -->          55092
-//    depth   8  :     0.021635 ms -->         390216
-//    depth   9  :     0.137550 ms -->        3005288
-//    depth  10  :     1.109636 ms -->       24571284
-//    depth  11  :     9.388059 ms -->      212258800
-//    depth  12  :    83.742958 ms -->     1939886636
-//    depth  13  :   782.551742 ms -->    18429641748
-//
-//    speed: 23.550.700 per second
-//
-//    see also http://www.aartbik.com/strategy.php
-//
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-func (bitBoard *BitBoard) perft(depth int, colorToMove int, justPassed bool) int64 {
-	if depth == 0 {
-		return 1
-	}
-	if bitBoard.AllFieldsPlayed() {
-		return 1
-	}
-	var nodeCount int64 = 0
-	positionList := bitBoard.GeneratePositions(colorToMove)
-	if len(positionList) == 0 {
-		if justPassed {
-			return 1
-		} else {
-			return bitBoard.perft(depth-1, 1-colorToMove, true)
-		}
-	} else {
-		for _, newPosition := range positionList {
-			nodeCount += newPosition.perft(depth-1, 1-colorToMove, false)
-		}
-	}
-	return nodeCount
 }
--- board/bitboard_test.go
+++ board/bitboard_test.go
@@ -8,25 +8,48 @@ import (
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//depth   1  :     0.000001 ms -->              4
-//depth   2  :     0.000002 ms -->             12
-//depth   3  :     0.000003 ms -->             56
-//depth   4  :     0.000016 ms -->            244
-//depth   5  :     0.000100 ms -->           1396
-//depth   6  :     0.000505 ms -->           8200
-//depth   7  :     0.003142 ms -->          55092
-//depth   8  :     0.020026 ms -->         390216
-//depth   9  :     0.131101 ms -->        3005288
-//depth  10  :     1.040574 ms -->       24571284
-//depth  11  :     8.783948 ms -->      212258800
-//depth  12  :    78.533921 ms -->     1939886636
-//depth  13  :   733.125194 ms -->    18429641748
+// depth   1  :     0.000001 ms -->              4
+// depth   2  :     0.000002 ms -->             12
+// depth   3  :     0.000003 ms -->             56
+// depth   4  :     0.000016 ms -->            244
+// depth   5  :     0.000100 ms -->           1396
+// depth   6  :     0.000505 ms -->           8200
+// depth   7  :     0.003142 ms -->          55092
+// depth   8  :     0.020026 ms -->         390216
+// depth   9  :     0.131101 ms -->        3005288
+// depth  10  :     1.040574 ms -->       24571284
+// depth  11  :     8.783948 ms -->      212258800
+// depth  12  :    78.533921 ms -->     1939886636
+// depth  13  :   733.125194 ms -->    18429641748
 //
 //    highest speed: 25.138.400 per second
 //
 //   see also http://www.aartbik.com/strategy.php
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func (bitBoard *BitBoard) perft(depth int, colorToMove int, justPassed bool) int64 {
+	if depth == 0 {
+		return 1
+	}
+	if bitBoard.AllFieldsPlayed() {
+		return 1
+	}
+	var nodeCount int64 = 0
+	positionList := bitBoard.GeneratePositions(colorToMove)
+	if len(positionList) == 0 {
+		if justPassed {
+			return 1
+		} else {
+			return bitBoard.perft(depth-1, 1-colorToMove, true)
+		}
+	} else {
+		for _, newPosition := range positionList {
+			nodeCount += newPosition.perft(depth-1, 1-colorToMove, false)
+		}
+	}
+	return nodeCount
+}
 
 func Test_tBitBoard_perft(t *testing.T) {
 	hb := InitStartBoard()
@@ -54,33 +77,6 @@ func Test_tBitBoard_perft(t *testing.T) {
 		}
 	}
 }
-
-//func Test_tBitBoard_perft(t *testing.T) {
-//	hb := InitStartBoard()
-//
-//	tables := []struct {
-//		x int
-//		n int64
-//	}{
-//		{1, 4},
-//		{2, 12},
-//		{3, 56},
-//		{4, 244},
-//		{5, 1396},
-//		{6, 8200},
-//		{7, 55092},
-//		{8, 390216},
-//		{9, 3005288},
-//		{10, 24571284},
-//	}
-//
-//	for _, table := range tables {
-//		nodeCount := hb.bitBoard.perft(table.x, black, false)
-//		if nodeCount != table.n {
-//			t.Errorf("Perft of %d was incorrect, got: %d, want: %d.", table.x, nodeCount, table.n)
-//		}
-//	}
-//}
 
 func Test_bitBoard_perft_print(t *testing.T) {
 	var hb = InitStartBoard()
